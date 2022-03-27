00b35ae04430b164e46ce8e28d6c924f90e1e4a6
sumye <wltnals00@naver.com>
2022-03-27 17:07:01 +0900

    회원가입기능까지 기본상태

---
+++ src/main/java/com/sparta/solo2/service/UserService.java
@@ -0,0 +1,44 @@
+package com.sparta.solo2.service;
+
+import com.sparta.solo2.dto.SignupRequestDto;
+import com.sparta.solo2.model.User;
+import com.sparta.solo2.model.UserRoleEnum;
+import com.sparta.solo2.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class UserService {
+    private final UserRepository userRepository;
+    private static final String ADMIN_TOKEN = "AAABnv/xRVklrnYxKZ0aHgTBcXukeZygoC";
+
+    @Autowired
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public void registerUser(SignupRequestDto requestDto) {
+        String username = requestDto.getUsername();
+        String password = requestDto.getPassword();
+// 회원 ID 중복 확인
+        Optional<User> found = userRepository.findByUsername(username);
+        if (found.isPresent()) {
+            throw new IllegalArgumentException("중복된 사용자 ID 가 존재합니다.");
+        }
+
+        String email = requestDto.getEmail();
+// 사용자 ROLE 확인
+        UserRoleEnum role = UserRoleEnum.USER;
+        if (requestDto.isAdmin()) {
+            if (!requestDto.getAdminToken().equals(ADMIN_TOKEN)) {
+                throw new IllegalArgumentException("관리자 암호가 틀려 등록이 불가능합니다.");
+            }
+            role = UserRoleEnum.ADMIN;
+        }
+
+        User user = new User(username, password, email, role);
+        userRepository.save(user);
+    }
+}
