ddce65793204faf275ff5ad4dadc79746e87d277
sumye <wltnals00@naver.com>
2022-03-31 14:44:34 +0900

    테스트코드 케이스 추가

--- src/test/java/com/sparta/solo2/service/UserServiceTest.java
+++ src/test/java/com/sparta/solo2/service/UserServiceTest.java
@@ -19,8 +19,7 @@ import org.springframework.stereotype.Service;
 import java.util.Optional;
 import java.util.regex.Pattern;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.*;
 
 
 @ExtendWith(MockitoExtension.class)
@@ -80,6 +79,45 @@ public class UserServiceTest {
 
 
             }
+
+        @Test
+        @DisplayName("아이디값이 세글자 미만으로 입력됫을때 유효성검사가 실패하는지")
+
+        void checkid2registerUser () {
+            //given
+            username = "as";
+            password = "asdf";
+            passwordcheck = "asdf";
+
+
+            SignupRequestDto requestDto = new SignupRequestDto(
+                    username, password, passwordcheck
+            );
+
+            UserService userService = new UserService(userRepository, passwordEncoder);
+            assertEquals("아이디를 확인해 주세요.", userService.registerUser(requestDto));
+
+
+        }
+        @Test
+        @DisplayName("아이디값이 아무값도 입력되지 않앗을때 유효성검사가 실패하는지")
+
+        void checkid3registerUser () {
+            //given
+            username = "";
+            password = "asdf";
+            passwordcheck = "asdf";
+
+
+            SignupRequestDto requestDto = new SignupRequestDto(
+                    username, password, passwordcheck
+            );
+
+            UserService userService = new UserService(userRepository, passwordEncoder);
+            assertEquals("아이디를 확인해 주세요.", userService.registerUser(requestDto));
+
+
+        }
 
         @Test
         @DisplayName("잘못된 비밀번호값이 입력됫을때 유효성검사가 실패하는지")
@@ -142,7 +180,7 @@ public class UserServiceTest {
         }
 
         @Test
-        @DisplayName("id중복 유효성 검사가 정상 작동하는지")
+        @DisplayName("데이터베이스에 있는 아이디로 회원가입시도 할때 회원가입실패하는지")
         void checkduplicationregisterUser() {
             MockUserRepository mockUserRepository = new MockUserRepository();
             //given
@@ -170,6 +208,39 @@ public class UserServiceTest {
                     username, password, passwordcheck
             );
             assertTrue(mockUserRepository.existsByUsername(requestDto.getUsername()) );
+
+
+        }
+
+        @Test
+        @DisplayName("데이터베이스에 없는 아이디로 회원가입할때 회원가입 성공하는지")
+        void checkduplication2registerUser() {
+            MockUserRepository mockUserRepository = new MockUserRepository();
+            //given
+            username = "wltnals00";
+            password = "asdf";
+            passwordcheck = "asdf";
+
+
+            SignupRequestDto requestDto = new SignupRequestDto(
+                    username, password, passwordcheck
+            );
+
+            User user = new User(
+                    requestDto.getUsername(),
+                    requestDto.getPassword()
+            );
+            mockUserRepository.save(user);
+
+            username = "akdntm161";
+            password = "1234";
+            passwordcheck = "1234";
+
+
+            requestDto = new SignupRequestDto(
+                    username, password, passwordcheck
+            );
+            assertFalse(mockUserRepository.existsByUsername(requestDto.getUsername()) );
 
 
         }
